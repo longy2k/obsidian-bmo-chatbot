99117fc55a29a4bb27a9abc333242ba6b998460e
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-18 19:23:24 -0400

    Successfully fetches openai api

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Configuration, OpenAIApi } from "openai";
 
 // Remember to rename these classes and interfaces!
 
 interface BMOSettings {
-	mySetting: string;
+	apiKey: string;
 }
 
 const DEFAULT_SETTINGS: BMOSettings = {
-	mySetting: 'default'
+	apiKey: ''
 }
 
 export default class BMOGPT extends Plugin {
 	settings: BMOSettings;
+	openai: OpenAIApi;
 
 	async onload() {
 		await this.loadSettings();
+
+		const configuration = new Configuration({
+			apiKey: this.settings.apiKey,
+		});
+		this.openai = new OpenAIApi(configuration);
+
+		this.addCommand({
+			id: "execute-note-prompt",
+			name: "Execute prompt (within current note)",
+			callback: this.handleChatbotCompletion.bind(this),
+		});
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('bot', 'BMO GPT-3.5-Turbo', (evt: MouseEvent) => {
@@ -28,61 +41,57 @@ export default class BMOGPT extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Tokens Used: 0');
 
-		// This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'execute-note-prompt',
-		// 	name: 'Execute prompt (within current note)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'execute-note-prompt',
-			name: 'Execute prompt (within current note)',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		//
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new BMOSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async onunload() {
+		await this.loadSettings();
 
+		const configuration = new Configuration({
+			apiKey: this.settings.apiKey,
+		});
+		this.openai = new OpenAIApi(configuration);
 	}
+
+	async handleChatbotCompletion() {
+	    if (!this.settings.apiKey) {
+	        new Notice("API key not found. Please add your OpenAI API key in the plugin settings.");
+	        return;
+	    }
+
+	    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+	    if (!view) {
+	        new Notice("No active Markdown view found.");
+	        return;
+	    }
+	    const editor = view.editor;
+
+	    try {
+	        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+	            method: 'POST',
+	            headers: {
+	                'Content-Type': 'application/json',
+	                'Authorization': `Bearer ${this.settings.apiKey}`
+	            },
+	            body: JSON.stringify({
+	                model: 'gpt-3.5-turbo',
+	                messages: [{ role: 'user', content: editor.getValue() }],
+	            }),
+	        });
+
+	        const data = await response.json();
+					console.log(data); // Log the response data to the console
+					console.log(editor.getValue());
+	        const message = data.choices[0].message.content;
+	        editor.replaceSelection(message);
+	    } catch (error) {
+	        new Notice('Error occurred while fetching completion: ' + error.message);
+	    }
+	}
+
+
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -93,21 +102,6 @@ export default class BMOGPT extends Plugin {
 	}
 }
 
-// class SampleModal extends Modal {
-// 	constructor(app: App) {
-// 		super(app);
-// 	}
-//
-// 	onOpen() {
-// 		const {contentEl} = this;
-// 		contentEl.setText('Woah!');
-// 	}
-//
-// 	onClose() {
-// 		const {contentEl} = this;
-// 		contentEl.empty();
-// 	}
-// }
 
 class BMOSettingTab extends PluginSettingTab {
 	plugin: BMOGPT;
@@ -122,17 +116,16 @@ class BMOSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for BMO-Obsdian-GPT'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('OpenAI API Key')
+			.setDesc('Insert API Key from OpenAI')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('OpenAI-api-key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
