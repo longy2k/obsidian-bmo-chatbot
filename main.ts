10fdc012bb4354040189cafc4d7356002b418cff
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-18 15:26:44 -0400

    Changed class name to fit BMO plugin name

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface BMOSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: BMOSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class BMOGPT extends Plugin {
+	settings: BMOSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -108,9 +108,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: BMOGPT;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: BMOGPT) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
