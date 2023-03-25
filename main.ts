4a568366235fb003f9960d0d5ac326bc51b699fe
Long Huynh <longhuynh.y2k@gmail.com>
2023-03-25 13:12:43 -0400

    Fixed main.ts typescript error

--- main.ts
+++ main.ts
@@ -111,8 +111,8 @@ export default class BMOGPT extends Plugin {
 										{ role: 'system', content: this.settings.system_role},
 										{ role: 'user', content: `${filename}\n\n${editor.getValue()}` }
 									],
-									max_tokens: parseInt(this.settings.max_tokens),
-									temperature: parseInt(this.settings.temperature),
+									max_tokens: this.settings.max_tokens,
+									temperature: this.settings.temperature,
 	            }),
 	        });
 
@@ -227,9 +227,9 @@ class BMOSettingTab extends PluginSettingTab {
 			.setDesc(descLink('When you chat with an AI, this setting controls the maximum length of the response it can generate. The response is broken down into small units called "tokens," and the maximum number of these tokens is limited to a specific number. (Max Token: 4096)', 'https://platform.openai.com/tokenizer'))
 			.addText(text => text
 				.setPlaceholder('4096')
-				.setValue(this.plugin.settings.max_tokens)
+				.setValue(`${this.plugin.settings.max_tokens}`)
 				.onChange(async (value) => {
-					this.plugin.settings.max_tokens = value;
+					this.plugin.settings.max_tokens = parseInt(value);
 					await this.plugin.saveSettings();
 				})
 		);
@@ -239,9 +239,9 @@ class BMOSettingTab extends PluginSettingTab {
 			.setDesc('Temperature is a setting in AI language models that controls how predictable or random the generated text is. Lower values (closer to 0) produce more predictable text, while higher values (closer to 2) result in more creative and unpredictable outputs.')
 			.addText(text => text
 				.setPlaceholder('1')
-				.setValue(this.plugin.settings.temperature)
+				.setValue(`${this.plugin.settings.temperature}`)
 				.onChange(async (value) => {
-					this.plugin.settings.temperature = value;
+					this.plugin.settings.temperature = parseFloat(value);
 					await this.plugin.saveSettings();
 				})
 		);
